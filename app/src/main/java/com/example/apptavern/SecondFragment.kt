984f5f08693d4c08f32dc0ffa501afd4054c4862
Seba <pezkaos@gmail.com>
2020-09-15 12:02:36 -0500

    avance 5

--- app/src/main/java/com/example/apptavern/SecondFragment.kt
+++ app/src/main/java/com/example/apptavern/SecondFragment.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
 
 import kotlinx.android.synthetic.main.fragment_second.*
 
@@ -39,4 +40,11 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
-}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        }
+
+    }
+
