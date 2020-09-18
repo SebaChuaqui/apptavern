d9b4750902b6f2b6fed4673f85ce59d84da5aa51
Seba <pezkaos@gmail.com>
2020-09-18 11:30:05 -0500

    avance 11 aun no funciona pero se puede

--- app/src/main/java/com/example/apptavern/model/DataBaseTavern.kt
+++ app/src/main/java/com/example/apptavern/model/DataBaseTavern.kt
@@ -1,6 +1,8 @@
 package com.example.apptavern.model
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 private const val DATA_BASE_NAME = "tavern_db"
@@ -10,5 +12,25 @@ abstract class DataBaseTavern:RoomDatabase(){
     abstract  fun getTavernDao(): TavernDao
     abstract fun getTavernInfo(): TavernInfoDao
 
-    
+    companion object{
+
+        @Volatile
+
+        private var INSTANCE: DataBaseTavern? = null
+
+        fun getDataBase(context: Context): DataBaseTavern{
+
+            val tempInstance = INSTANCE
+            if(tempInstance !=null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(context, DataBaseTavern::class.java, DATA_BASE_NAME).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+
 }
